d38639f0a3cdb5090711179d0e78df857ecbd5d3
Jordan Harband <ljharb@gmail.com>
2019-12-01 23:42:56 -0800

    [Refactor] use `side-channel` package

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
 'use strict';
 
 var GetIntrinsic = require('es-abstract/GetIntrinsic');
-var callBound = require('es-abstract/helpers/callBound');
 var has = require('has');
+var channel = require('side-channel')();
 
 var $TypeError = GetIntrinsic('%TypeError%');
-var $WeakMap = GetIntrinsic('%WeakMap%', true);
-var $Map = GetIntrinsic('%Map%', true);
-var $push = callBound('Array.prototype.push');
-
-var $mapGet = callBound('WeakMap.prototype.get', true)
-	|| callBound('Map.prototype.get', true)
-	|| function (objects, O) { // eslint-disable-line consistent-return
-		for (var i = 0; i < objects.length; i += 1) {
-			if (objects[i].O === O) {
-				return objects[i].V;
-			}
-		}
-	};
-var $mapSet = callBound('WeakMap.prototype.set', true)
-	|| callBound('Map.prototype.set', true)
-	|| function (objects, O, slots) {
-		for (var i = 0; i < objects.length; i += 1) {
-			if (objects[i].O === O) {
-				objects[i].V = slots;
-				return;
-			}
-		}
-		$push(objects, {
-			O: O,
-			V: slots
-		});
-	};
-var map = ($WeakMap || $Map) ? new ($WeakMap || $Map)() : []; // eslint-disable-line no-extra-parens
 
 var SLOT = {
 	assert: function (O, slot) {
@@ -42,9 +14,7 @@ var SLOT = {
 		if (typeof slot !== 'string') {
 			throw new $TypeError('`slot` must be a string');
 		}
-		if (!SLOT.has(O, slot)) {
-			throw new $TypeError('Object lacks internal slot ' + slot);
-		}
+		channel.assert(O);
 	},
 	get: function (O, slot) {
 		if (!O || (typeof O !== 'object' && typeof O !== 'function')) {
@@ -53,7 +23,7 @@ var SLOT = {
 		if (typeof slot !== 'string') {
 			throw new $TypeError('`slot` must be a string');
 		}
-		var slots = $mapGet(map, O);
+		var slots = channel.get(O);
 		return slots && slots['$' + slot];
 	},
 	has: function (O, slot) {
@@ -63,8 +33,8 @@ var SLOT = {
 		if (typeof slot !== 'string') {
 			throw new $TypeError('`slot` must be a string');
 		}
-		var slots = $mapGet(map, O) || {};
-		return has(slots, '$' + slot);
+		var slots = channel.get(O);
+		return !!slots && has(slots, '$' + slot);
 	},
 	set: function (O, slot, V) {
 		if (!O || (typeof O !== 'object' && typeof O !== 'function')) {
@@ -73,10 +43,10 @@ var SLOT = {
 		if (typeof slot !== 'string') {
 			throw new $TypeError('`slot` must be a string');
 		}
-		var slots = $mapGet(map, O);
+		var slots = channel.get(O);
 		if (!slots) {
 			slots = {};
-			$mapSet(map, O, slots);
+			channel.set(O, slots);
 		}
 		slots['$' + slot] = V;
 	}
